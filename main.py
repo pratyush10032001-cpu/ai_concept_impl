bf4576432ac2a6573211dd81375e649f2aaedcda
pratyush10032001-cpu <pratyush10032001@gmail.com>
2026-04-06 17:01:41 +0530

    added runnables and fast api impl

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from ai-impl!")
+from langserve import add_routes
+from fastapi import FastAPI
+from pydantic import BaseModel
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+class TranslationRequest(BaseModel):
+    lang:str
+    text:str
+
+@app.get("/hello")
+def hello():
+    return {"message":"Hello, How are you?"}
+
+@app.post("/llm")
+def translationLang(request:TranslationRequest):
+    llm = ChatGroq(model="llama-3.1-8b-instant")
+
+    prompt = PromptTemplate(template='Translate the text: {text} from english to {lang}',input_variables=['text','lang'])
+
+    parser = StrOutputParser()
+
+    chain = prompt | llm | parser
+
+    res = chain.invoke({'text':request.text,'lang':request.lang})
+
+    return {'translation':res}
